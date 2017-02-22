66dfba5e797de34260e8e8c3c091e9b07eea58d6
Cyprien Dcunha <dcunha.cyprien@gmail.com>
2017-02-22 10:34:29 +0530

    Add RBM with Tensorflow for Image Reconstruction (Restricted Boltzmann Machine + MNIST images) * similar to numpy code - however this tensorflow code runs 0.25x faster

---
+++ rbm/mnist-reconstruction/tf_rbm_mnist.py
@@ -0,0 +1,129 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+#################### Util Functions ####################
+
+def plot_xy_images(images, title, no_i_x, no_i_y):
+    fig = plt.figure()
+    fig.canvas.set_window_title(title)
+    images = np.array(images).reshape(-1, 28, 28)
+    for i in range(no_i_x):
+        for j in range(no_i_y):
+            ax = fig.add_subplot(no_i_x, no_i_y, no_i_x * j + (i + 1))
+            ax.matshow(images[no_i_x * j + i], cmap="gray")
+            plt.xticks(np.array([]))
+            plt.yticks(np.array([]))
+
+
+#################### Preprocess data ####################
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+features_train, labels_train, features_test, labels_test = \
+    mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
+############# Neural Network (Training Graph) #############
+
+# Parameters
+n_input_layer = features_train.shape[1]
+n_hidden_layer = 500
+
+learning_rate = 1.0
+
+n_epoch = 10
+batch_size = 100
+
+# no of images in plot (X, Y co-ordinates)
+test_x = 4
+test_y = 4
+
+# Placeholders
+X = tf.placeholder(tf.float32, [None, n_input_layer])
+W = tf.placeholder(tf.float32, [n_input_layer, n_hidden_layer])
+B = tf.placeholder(tf.float32, [n_hidden_layer])
+C = tf.placeholder(tf.float32, [n_input_layer])
+
+
+# Get sample from input
+def get_sample(inp):
+    distr = tf.random_uniform(shape=tf.shape(inp))
+    sample = tf.nn.relu(tf.sign(inp - distr))
+    return sample
+
+
+# Forward pass
+p_h = tf.nn.sigmoid(tf.add(tf.matmul(X, W), B))
+s_h = get_sample(p_h)
+
+# Backward pass
+p_v = tf.nn.sigmoid(tf.add(tf.matmul(s_h, tf.transpose(W)), C))
+s_v = get_sample(p_v)
+
+p_h1 = tf.nn.sigmoid(tf.add(tf.matmul(s_v, W), B))
+
+# Error function
+error = 0.5 * tf.reduce_mean(tf.square(tf.sub(s_v, X)))
+
+# Positive phase grad
+p_w_change = tf.matmul(tf.transpose(X), s_h)
+
+# Negative phase grad
+n_w_change = tf.matmul(tf.transpose(s_v), p_h1)
+
+contr_div = tf.sub(p_w_change, n_w_change) / tf.to_float(tf.shape(X)[0])
+
+change_w = contr_div
+change_c = tf.reduce_mean(tf.sub(X, s_v), 0)
+change_b = tf.reduce_mean(tf.sub(s_h, p_h1), 0)
+
+# Adjust Weights
+new_W = W + learning_rate * change_w
+new_B = B + learning_rate * change_b
+new_C = C + learning_rate * change_c
+
+########### Neural Network (Reconstruction Graph) ###########
+
+hid = tf.nn.sigmoid(tf.add(tf.matmul(X, W), B))
+rc = tf.nn.sigmoid(tf.add(tf.matmul(hid, tf.transpose(W)), C))
+
+#################### Train Neural Network ####################
+
+# Initialize random  Weights and biases
+w = np.random.uniform(0.1, size=(n_input_layer, n_hidden_layer))
+b = np.random.uniform(0.1, size=n_hidden_layer)
+c = np.random.uniform(0.1, size=n_input_layer)
+
+# Make Batches
+n_batch = features_train.shape[0] // batch_size
+batched_data = np.split(features_train, n_batch)
+
+# Start session
+with tf.Session() as sess:
+    tf.global_variables_initializer().run()
+
+    # Epoch-training
+    for epoch in range(n_epoch):
+        err = []
+
+        # Batch training
+        for b_idx in range(n_batch):
+            e, w, b, c = sess.run([error, new_W, new_B, new_C], feed_dict={
+                X: batched_data[b_idx], W: w, B: b, C: c})
+
+            err.append(e)
+
+        print("Epoch: %d, Error: %.8f" % (epoch, sum(err) / len(err)))
+
+    # Reconstruction
+    img = []
+    test_cases = test_x * test_y
+    for i_no in range(test_cases):
+        reconstructed_image = sess.run(rc, feed_dict={
+            X: features_test[i_no].reshape(1, -1), W: w, B: b, C: c})
+        img.append(reconstructed_image)
+
+    plot_xy_images(img, "Reconstructed MNIST Data", test_x, test_y)
+    plot_xy_images(features_test[:test_cases], "Original MNIST Data", test_x, test_y)
+    plt.show()
